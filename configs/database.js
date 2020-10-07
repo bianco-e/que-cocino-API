76ed171329a6a5118a578573be47fb3db530ad86
Bianco <bianco.emiliozzi@gmail.com>
2020-10-07 14:24:29 -0300

    :tada:

--- configs/database.js
+++ configs/database.js
@@ -1,3 +1,1 @@
-const KEY = require("./apikey");
-
-module.exports = `mongodb+srv://admin-whattocook:${KEY}@cluster0.cv5ri.mongodb.net/whattocookAPI?retryWrites=true&w=majority`;
+module.exports = `mongodb+srv://admin-whattocook:${process.env.APIKEY}@cluster0.cv5ri.mongodb.net/whattocookAPI?retryWrites=true&w=majority`;
