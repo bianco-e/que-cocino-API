c25bb15cb48292b8668ef527b8362703cac81971
Bianco <bianco.emiliozzi@gmail.com>
2020-10-07 12:38:30 -0300

    :tada:

--- routes/routes.js
+++ routes/routes.js
@@ -28,16 +28,34 @@ router.post("/recipes/filter", (req, res) => {
   );
 });
 
+router.post("/recipes/add", (req, res) => {
+  const recipe = new Recipe(req.body);
+  recipe.save((err, addedRecipe) => {
+    if (err) return console.log(err);
+    res.header("Access-Control-Allow-Origin", "*");
+    res.status(200).send("Added" + addedRecipe);
+    res.end();
+  });
+});
+
+router.post("/ingredients/add", (req, res) => {
+  const ingredient = new Ingredient(req.body);
+  ingredient.save((err, addedIngredient) => {
+    if (err) return console.log(err);
+    res.header("Access-Control-Allow-Origin", "*");
+    res.status(200).send("Added" + addedIngredient);
+    res.end();
+  });
+});
+
 router.get("/ingredients/contains/:keyword", (req, res) => {
   const { keyword } = req.params;
-  Ingredient.find(
-    { name: { $regex: keyword, $options: "i" } },
-    { _id: 0 },
-    (err, docs) => {
+  Ingredient.find({ name: { $regex: keyword, $options: "i" } }, { _id: 0 })
+    .limit(5)
+    .exec((err, docs) => {
       if (err) return console.log(err);
       res.send(docs.map((ingredient) => ingredient.name));
-    }
-  );
+    });
 });
 
 module.exports = router;
