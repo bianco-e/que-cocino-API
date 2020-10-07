27b89b5f8a0030999258f488526080ce736f3b04
Bianco <bianco.emiliozzi@gmail.com>
2020-10-07 14:39:03 -0300

    :tada:

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const dbURL = require("./configs/database");
 const routes = require("./routes/routes");
 
 const app = express();
-app.set("port", 5000);
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(routes);
@@ -18,4 +18,4 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => console.log(`Connected to '${db.name}' db`));
 
-app.listen(app.get("port"), () => console.log(`On port ${app.get("port")}`));
+app.listen(PORT, () => console.log(`On port ${PORT}`));
